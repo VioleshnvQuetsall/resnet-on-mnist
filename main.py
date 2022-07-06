e8cbe0f8c736e5a85cf96ec746b78d6d0b9fdd13
VioleshnvQuetsall <violeshnv@163.com>
2022-07-06 10:55:45 +0800

    update logger format in main.py

--- main.py
+++ main.py
@@ -81,10 +81,10 @@ def train_model(model: nn.Module,
             torch.save(model.state_dict(), save_path)
             print(f"Saved PyTorch Model State to {save_path}")
 
-    logger.info(f'    test loss: {test_loss}')
-    logger.info(f'    test correct: {test_correct}')
-    logger.info(f'    trigger loss: {trigger_loss}')
-    logger.info(f'    trigger correct: {trigger_correct}')
+    logger.info(f'  test loss: {test_loss}')
+    logger.info(f'  test correct: {test_correct}')
+    logger.info(f'  trigger loss: {trigger_loss}')
+    logger.info(f'  trigger correct: {trigger_correct}')
     # logger.info(f"{'Trigger' if with_trigger else 'Prepare'} Done!")
 
     return model
